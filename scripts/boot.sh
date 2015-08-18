c9d81a1be1d9c976f98232edc26f3140d9a06a16
Kyle Davis <validkeys@gmail.com>
2015-08-18 11:51:14 -0400

    Updated supervisor, boot script and added start redis script

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -1,5 +1,5 @@
 # START REDIS (daemon)
-docker run -v ~/Documents/dockermnt/data/redis:/data/redis -p 6379:6379 --name REDIS -d stats-redis
+docker run -v /mnt/data:/data -p 6379:6379 --name REDIS -d stats-redis
 
 # START MONGO
-docker run -v ~/Documents/dockermnt/data:/data -v ~/Documents/dockermnt/logs:/logs -p 27017:27017 --name MONGO -d stats-mongo
+docker run -v /mnt/data:/data -v /mnt/logs:/logs -p 27017:27017 --name MONGO -d stats-mongo
---
+++ scripts/start_redis.sh
@@ -0,0 +1,1 @@
+docker run -v /mnt/data:/data -p 6379:6379 --name REDIS -d stats-redis
