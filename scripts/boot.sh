8374830312c61e7b4ea3702f3c353cc162e4aaea
Kyle Davis <validkeys@gmail.com>
2015-08-18 12:16:37 -0400

    updated start app with links

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -5,4 +5,4 @@ docker run -v /mnt/data:/data -p 6379:6379 --name REDIS -d stats-redis
 docker run -v /mnt/data:/data -v /mnt/logs:/logs -p 27017:27017 --name MONGO -d stats-mongo
 
 # START APP
-docker run -v /mnt/logs:/logs -p 80:3000 --name API -d stats-api
+docker run -v /mnt/logs:/logs -p 80:3000 --link REDIS:redis --link MONGO:mongo --name API -d stats-api
