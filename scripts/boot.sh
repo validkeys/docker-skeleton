087da34ffa8a13d365321ecd4e713663136c183e
Kyle Davis <validkeys@gmail.com>
2015-08-18 12:05:39 -0400

    Added start app script

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -3,3 +3,6 @@ docker run -v /mnt/data:/data -p 6379:6379 --name REDIS -d stats-redis
 
 # START MONGO
 docker run -v /mnt/data:/data -v /mnt/logs:/logs -p 27017:27017 --name MONGO -d stats-mongo
+
+# START APP
+docker run -v /mnt/logs:/logs -p 80:3000 --name API -d stats-api
---
+++ scripts/start_app.sh
@@ -0,0 +1,1 @@
+docker run -v /mnt/logs:/logs -p 80:3000 --name API -d stats-api
