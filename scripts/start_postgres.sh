3e27ec63b44099ae2718f37fc36b5ee0690584d0
kyle davis <validkeys@gmail.com>
2015-08-18 23:13:52 -0400

    Added port forwarding

--- scripts/start_postgres.sh
+++ scripts/start_postgres.sh
@@ -1,1 +1,1 @@
-docker run -v /mnt/logs/postgresql:/var/log -v /mnt/data/postgresql:/var/lib/postgresql/data  --name POSTGRESQL -d stats-postgres
+docker run -v /mnt/logs/postgresql:/var/log -v /mnt/data/postgresql:/var/lib/postgresql/data -p 5432:5432 --name POSTGRESQL -d stats-postgres
