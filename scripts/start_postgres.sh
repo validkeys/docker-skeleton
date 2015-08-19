53f7d4e3d53760492033f19764d8565c887dc998
Kyle Davis <validkeys@gmail.com>
2015-08-19 09:42:41 -0400

    finalized pg docker

--- scripts/start_postgres.sh
+++ scripts/start_postgres.sh
@@ -1,1 +1,1 @@
-docker run -v /mnt/logs/postgresql:/var/log -v /mnt/data/postgresql:/var/lib/postgresql/data -p 5432:5432 --name POSTGRESQL -d stats-postgres /usr/lib/supervisord
+docker run -v /mnt/logs/postgresql:/var/log -v /mnt/data/postgresql:/var/lib/postgresql/data -p 5432:5432 --name POSTGRESQL -d stats-postgres
